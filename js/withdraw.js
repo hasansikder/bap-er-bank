bc61f481bb61089cdb72c4ef53919c28fd7a59cb
HasanSikder <mdhasansikder16@gmail.com>
2022-12-07 12:11:00 +0600

    Completed

--- js/withdraw.js
+++ js/withdraw.js
@@ -18,22 +18,37 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithDrawAmountString = withdrawField.value;
     const newWithDrawAmount = parseFloat(newWithDrawAmountString);
+    
+    //step-7
+    withdrawField.value = '';
+
+    if (isNaN(newWithDrawAmount)) {
+        alert('Please Provide a valid number');
+        return;
+    }
     //step-3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    // step-4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+
 
     //setp-5
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    if (newWithDrawAmount > previousBalanceTotal) {
+        alert('Bank e Eto Taka Nai');
+        return;
+    }
+
+    // step-4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     //step-6
     const newBalanceTotal = previousBalanceTotal - newWithDrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
-    //step-7
-    withdrawField.value = '';
+
 
 })
